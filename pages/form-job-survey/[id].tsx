63ea0d0bca95b4a3b65509e1db9e6469c965d5b3
minhlh <hongminhle290@gmail.com>
2024-11-14 21:46:06 +0700

    feat: add log

--- pages/form-job-survey/[id].tsx
+++ pages/form-job-survey/[id].tsx
@@ -11,47 +11,43 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const trainingIndustryService = useTrainingIndustryService();
   const cityService = useCityService();
 
-  if (typeof id === 'string') {
-    try {
-      const res = await surveyPeriodService.getSurveyPeriod(id);
-      if (!res.data) {
-        throw new Error('Survey period not found');
-      }
-      const surveyPeriod = res.data.data;
-
-      const trainingIndustryRes = await trainingIndustryService.getList({
-        faculty_id: surveyPeriod.faculty_id,
-      });
-
-      const dataOptionTrainingIndustries = trainingIndustryRes.data.data.map((item) => ({
-        label: item.name,
-        value: String(item.id),
-      }));
-
-      const cityRes = await cityService.getList();
-
-      const dataOptionCities = cityRes.data.data.map((item) => ({
-        label: item.name,
-        value: String(item.id),
-      }));
-
-      return {
-        props: {
-          layout: 'unLoggedIn',
-          surveyPeriod,
-          dataOptionTrainingIndustries,
-          dataOptionCities,
-        },
-      };
-    } catch (error) {
-      return {
-        notFound: true,
-      };
+  try {
+    const res = await surveyPeriodService.getSurveyPeriod(String(id));
+    if (!res.data) {
+      throw new Error('Survey period not found');
     }
+    const surveyPeriod = res.data.data;
+
+    const trainingIndustryRes = await trainingIndustryService.getList({
+      faculty_id: surveyPeriod.faculty_id,
+    });
+
+    const dataOptionTrainingIndustries = trainingIndustryRes.data.data.map((item) => ({
+      label: item.name,
+      value: String(item.id),
+    }));
+
+    const cityRes = await cityService.getList();
+
+    const dataOptionCities = cityRes.data.data.map((item) => ({
+      label: item.name,
+      value: String(item.id),
+    }));
+
+    return {
+      props: {
+        layout: 'unLoggedIn',
+        surveyPeriod,
+        dataOptionTrainingIndustries,
+        dataOptionCities,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      notFound: true,
+    };
   }
-  return {
-    notFound: true,
-  };
 };
 
 export const getStaticPaths = async () => ({
