35834f1534afe84587be20a0cc42a9a3e4cf5673
chiendd <chiendam1908@gmail.com>
2024-11-18 15:11:13 +0700

    feat: disable input info private

--- services/employmentSurveyResponseService.ts
+++ services/employmentSurveyResponseService.ts
@@ -8,6 +8,11 @@ export const useEmploymentSurveyResponse = () => {
   ): Promise<AxiosResponse<ResultResonse<FormJobSurvey>, any>> =>
     axiosInstance.post('/external/employment-survey-response', formJobSurvey);
 
+  const updateResponse = (
+    formResponse: FormJobSurvey
+  ): Promise<AxiosResponse<ResultResonse<FormJobSurvey>, any>> =>
+    axiosInstance.patch(`/external/employment-survey-response/${formResponse.id}`, formResponse);
+
   const getResponse = (params: {
     student_code?: string;
     survey_period_id: number;
@@ -18,5 +23,6 @@ export const useEmploymentSurveyResponse = () => {
   return {
     createResponse,
     getResponse,
+    updateResponse,
   };
 };
